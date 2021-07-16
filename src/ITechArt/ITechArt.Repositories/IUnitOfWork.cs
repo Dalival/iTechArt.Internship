61d1661e09cba83fad9e3d90dc9281371b7cf03d
Egor <egorfedorenko.w@gmail.com>
2021-07-16 10:06:39 +0300

    Delete whitespace

--- src/ITechArt/ITechArt.Repositories/IUnitOfWork.cs
+++ src/ITechArt/ITechArt.Repositories/IUnitOfWork.cs
@@ -5,7 +5,7 @@ namespace ITechArt.Repositories
 {
     public interface IUnitOfWork : IDisposable
     {
-         IRepository<TEntity> GetRepository<TEntity>()
+        IRepository<TEntity> GetRepository<TEntity>()
             where TEntity : class;
 
         Task CommitAsync();
