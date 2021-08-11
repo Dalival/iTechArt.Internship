70a0f0dcff0863cd7ecdb566769ae7992feba5cc
Egor <egorfedorenko.w@gmail.com>
2021-08-11 16:44:50 +0300

    Added NLog, it's config, updated appsettings.json

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Program.cs
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Program.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace ITechArt.Surveys.WebApp
 {
@@ -7,7 +10,21 @@ namespace ITechArt.Surveys.WebApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -15,6 +32,12 @@ namespace ITechArt.Surveys.WebApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
