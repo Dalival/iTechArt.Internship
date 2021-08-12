bb83eff7ec5bb4698d678e5a2bb2d2464cadaea8
Egor <egorfedorenko.w@gmail.com>
2021-08-12 14:05:30 +0300

    Simplified CustomLogger class and interface

--- src/ITechArt/ITechArt.Common/Logger/CustomLogger.cs
+++ src/ITechArt/ITechArt.Common/Logger/CustomLogger.cs
@@ -14,82 +14,22 @@ namespace ITechArt.Common.Logger
         }
 
 
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return _logger.IsEnabled(logLevel);
-        }
-
-        public void Log<TState>(LogLevel logLevel, TState state, Exception exception,
-            Func<TState, Exception, string> formatter)
-        {
-            throw new NotImplementedException();
-        }
-
         public void Log(LogLevel logLevel, Exception exception, string message, params object[] args)
         {
-            _logger.Log(logLevel, exception, message, args);
-        }
-
-        public void Log(LogLevel logLevel, string message, params object[] args)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void LogCritical(Exception exception, string message, params object[] args)
-        {
-            _logger.LogCritical(exception, message, args);
-        }
-
-        public void LogCritical(string message, params object[] args)
-        {
-            _logger.LogCritical(message, args);
-        }
-
-        public void LogError(Exception exception, string message, params object[] args)
-        {
-            _logger.LogError(exception, message, args);
             var microsoftLogLevel = ConvertLogLevel(logLevel);
             _logger.Log(microsoftLogLevel, exception, message, args);
         }
 
-        public void LogError(string message, params object[] args)
-        {
-            _logger.LogError(message, args);
-        }
 
-        public void LogWarning(Exception exception, string message, params object[] args)
-        {
-            _logger.LogWarning(exception, message, args);
-        }
-
-        public void LogWarning(string message, params object[] args)
-        {
-            _logger.LogWarning(message, args);
-        }
-
-        public void LogInformation(Exception exception, string message, params object[] args)
-        {
-            _logger.LogInformation(exception, message, args);
-        }
-
-        public void LogInformation(string message, params object[] args)
         public void Log(LogLevel logLevel, string message, params object[] args)
         {
-            _logger.LogInformation(message, args);
             var microsoftLogLevel = ConvertLogLevel(logLevel);
             _logger.Log(microsoftLogLevel, message, args);
         }
 
-        public void LogDebug(Exception exception, string message, params object[] args)
-        {
-            _logger.LogDebug(exception, message, args);
-        }
 
-        public void LogDebug(string message, params object[] args)
         private Microsoft.Extensions.Logging.LogLevel ConvertLogLevel(LogLevel sourceLevel)
         {
-            _logger.LogDebug(message, args);
-        }
             var destinationLevel = sourceLevel switch
             {
                 LogLevel.Trace => Microsoft.Extensions.Logging.LogLevel.Trace,
@@ -101,14 +41,6 @@ namespace ITechArt.Common.Logger
                 _ => throw new ArgumentOutOfRangeException(nameof(sourceLevel), sourceLevel, null)
             };
 
-        public void LogTrace(Exception exception, string message, params object[] args)
-        {
-            _logger.LogTrace(exception, message, args);
-        }
-
-        public void LogTrace(string message, params object[] args)
-        {
-            _logger.LogTrace(message, args);
             return destinationLevel;
         }
     }
--- src/ITechArt/ITechArt.Common/Logger/ICustomLogger.cs
+++ src/ITechArt/ITechArt.Common/Logger/ICustomLogger.cs
@@ -1,41 +1,13 @@
 ﻿using System;
-using Microsoft.Extensions.Logging;
 
 namespace ITechArt.Common.Logger
 {
     public interface ICustomLogger
     {
-        bool IsEnabled(LogLevel logLevel);
-
-        void Log<TState>(LogLevel logLevel, TState state, Exception exception,
-            Func<TState, Exception, String> formatter);
-
-        void Log(LogLevel logLevel, Exception exception, string message, params object[] args);
-
-        void Log(LogLevel logLevel, string message, params object[] args);
-
-        void LogCritical(Exception exception, string message, params object[] args);
-
-        void LogCritical(string message, params object[] args);
-
-        void LogError(Exception exception, string message, params object[] args);
-
-        void LogError(string message, params object[] args);
-
-        void LogWarning(Exception exception, string message, params object[] args);
-
-        void LogWarning(string message, params object[] args);
-
-        void LogInformation(Exception exception, string message, params object[] args);
-
-        void LogInformation(string message, params object[] args);
-
-        void LogDebug(Exception exception, string message, params object[] args);
-
-        void LogDebug(string message, params object[] args);
-
-        void LogTrace(Exception exception, string message, params object[] args);
-
-        void LogTrace(string message, params object[] args);
+        void Log(
+            LogLevel logLevel,
+            Exception exception,
+            string message,
+            params object[] args);
     }
 }
