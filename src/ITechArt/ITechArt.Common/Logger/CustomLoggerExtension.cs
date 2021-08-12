4294f577a0564e182263cb463d9daf2734402de2
Egor <egorfedorenko.w@gmail.com>
2021-08-12 15:05:09 +0300

    Implemented methods in CustomLoggerExtension

--- src/ITechArt/ITechArt.Common/Logger/CustomLoggerExtension.cs
+++ src/ITechArt/ITechArt.Common/Logger/CustomLoggerExtension.cs
@@ -7,67 +7,67 @@ namespace ITechArt.Common.Logger
         public static void LogCritical(this ICustomLogger logger, Exception exception, string message,
             params object[] args)
         {
-            _logger.LogCritical(exception, message, args);
+            logger.Log(LogLevel.Critical, exception, message, args);
         }
 
         public static void LogCritical(this ICustomLogger logger, string message, params object[] args)
         {
-            _logger.LogCritical(message, args);
+            logger.Log(LogLevel.Critical, null, message, args);
         }
 
         public static void LogError(this ICustomLogger logger, Exception exception, string message,
             params object[] args)
         {
-            _logger.LogError(exception, message, args);
+            logger.Log(LogLevel.Error, exception, message, args);
         }
 
         public static void LogError(this ICustomLogger logger, string message, params object[] args)
         {
-            _logger.LogError(message, args);
+            logger.Log(LogLevel.Error, null, message, args);
         }
 
         public static void LogWarning(this ICustomLogger logger, Exception exception, string message,
             params object[] args)
         {
-            _logger.LogWarning(exception, message, args);
+            logger.Log(LogLevel.Warning, exception, message, args);
         }
 
         public static void LogWarning(this ICustomLogger logger, string message, params object[] args)
         {
-            _logger.LogWarning(message, args);
+            logger.Log(LogLevel.Warning, null, message, args);
         }
 
         public static void LogInformation(this ICustomLogger logger, Exception exception, string message,
             params object[] args)
         {
-            _logger.LogInformation(exception, message, args);
+            logger.Log(LogLevel.Information, exception, message, args);
         }
 
         public static void LogInformation(this ICustomLogger logger, string message, params object[] args)
         {
-            _logger.LogInformation(message, args);
+            logger.Log(LogLevel.Information, null, message, args);
         }
 
         public static void LogDebug(this ICustomLogger logger, Exception exception, string message,
             params object[] args)
         {
-            _logger.LogDebug(exception, message, args);
+            logger.Log(LogLevel.Debug, exception, message, args);
         }
 
         public static void LogDebug(this ICustomLogger logger, string message, params object[] args)
         {
-            _logger.LogDebug(message, args);
+            logger.Log(LogLevel.Debug, null, message, args);
         }
 
         public static void LogTrace(this ICustomLogger logger, Exception exception, string message,
             params object[] args)
         {
-            _logger.LogTrace(exception, message, args);
+            logger.Log(LogLevel.Trace, exception, message, args);
         }
 
         public static void LogTrace(this ICustomLogger logger, string message, params object[] args)
         {
-            _logger.LogTrace(message, args);
+            logger.Log(LogLevel.Trace, null, message, args);
         }
     }
 }
