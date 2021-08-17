7fc2dd60421e03863c57bda1b806046b5b223fcc
Egor <egorfedorenko.w@gmail.com>
2021-08-17 12:26:46 +0300

    Revert "Configured Identity in Startup"
    This reverts commit 909601070bdd0e5080822d563b3f6264cab96a4b.

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Startup.cs
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Startup.cs
@@ -11,7 +11,6 @@ using ITechArt.Surveys.Foundation;
 using ITechArt.Surveys.Foundation.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using ITechArt.Surveys.DomainModel;
-using System;
 
 namespace ITechArt.Surveys.WebApp
 {
@@ -40,34 +39,6 @@ namespace ITechArt.Surveys.WebApp
             services.AddScoped<ICounterService, CounterService>();
 
             services.AddMvc();
-
-            services.Configure<IdentityOptions>(options =>
-            {
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
-
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                options.Lockout.MaxFailedAccessAttempts = 10;
-                options.Lockout.AllowedForNewUsers = true;
-
-                options.User.AllowedUserNameCharacters =
-                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                options.User.RequireUniqueEmail = true;
-            });
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
-                options.LoginPath = "/Identity/Account/Login";
-                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-                options.SlidingExpiration = true;
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -89,7 +60,6 @@ namespace ITechArt.Surveys.WebApp
 
             app.UseRouting();
 
-            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
