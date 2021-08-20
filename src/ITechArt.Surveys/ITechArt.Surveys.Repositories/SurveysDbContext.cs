d0c8f9b660f85d37e4f527c288521d1822ea2515
Egor <egorfedorenko.w@gmail.com>
2021-08-20 10:02:41 +0300

    Updated DbContext for identity

--- src/ITechArt.Surveys/ITechArt.Surveys.Repositories/SurveysDbContext.cs
+++ src/ITechArt.Surveys/ITechArt.Surveys.Repositories/SurveysDbContext.cs
@@ -5,6 +5,13 @@ namespace ITechArt.Surveys.Repositories
 {
     public class SurveysDbContext : DbContext
     {
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+
         public SurveysDbContext(DbContextOptions options)
             : base(options) { }
 
@@ -14,6 +21,41 @@ namespace ITechArt.Surveys.Repositories
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Counter>().HasKey(c => c.Id);
+
+            modelBuilder.Entity<User>(b =>
+            {
+                b.HasKey(u => u.Id);
+                b.HasIndex(u => u.NormalizedUserName).HasDatabaseName("UserNameIndex").IsUnique();
+                b.HasIndex(u => u.NormalizedEmail).HasDatabaseName("EmailIndex"); // why not to make Email unique too?
+                b.ToTable("Users");
+                b.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+                b.Property(u => u.UserName).HasMaxLength(256);
+                b.Property(u => u.NormalizedUserName).HasMaxLength(256);
+                b.Property(u => u.Email).HasMaxLength(256);
+                b.Property(u => u.NormalizedEmail).HasMaxLength(256);
+
+                b.HasMany<UserRole>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
+            });
+
+            modelBuilder.Entity<Role>(b =>
+            {
+                b.HasKey(r => r.Id);
+                b.HasIndex(r => r.NormalizedName).HasDatabaseName("RoleNameIndex").IsUnique();
+                b.ToTable("Roles");
+                b.Property(r => r.ConcurrencyStamp).IsConcurrencyToken();
+
+                b.Property(u => u.Name).HasMaxLength(256);
+                b.Property(u => u.NormalizedName).HasMaxLength(256);
+
+                b.HasMany<UserRole>().WithOne().HasForeignKey(ur => ur.RoleId).IsRequired();
+            });
+
+            modelBuilder.Entity<UserRole>(b =>
+            {
+                b.HasKey(r => new { r.UserId, r.RoleId });
+                b.ToTable("UserRoles");
+            });
         }
     }
 }
