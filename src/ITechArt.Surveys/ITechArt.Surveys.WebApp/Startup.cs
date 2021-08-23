325af00bf0412a637976eace6fb184ac26d14d7f
Egor <egorfedorenko.w@gmail.com>
2021-08-23 18:15:55 +0300

    Configured Identity options in Startup

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Startup.cs
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using ITechArt.Common.Logger;
 using ITechArt.Repositories;
 using Microsoft.AspNetCore.Builder;
@@ -42,6 +43,34 @@ namespace ITechArt.Surveys.WebApp
             services.AddScoped<ICustomLogger, CustomLogger>();
 
             services.AddControllersWithViews();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
