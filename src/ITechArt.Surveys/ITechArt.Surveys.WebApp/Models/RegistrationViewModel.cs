c6444136fac6be8c6c005250b46a8ac5ae09f1c4
Egor <egorfedorenko.w@gmail.com>
2021-08-29 18:02:07 +0300

    RegistrationViewModel and view updated

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Models/RegistrationViewModel.cs
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/Models/RegistrationViewModel.cs
@@ -6,18 +6,25 @@ namespace ITechArt.Surveys.WebApp.Models
     public class RegistrationViewModel
     {
         [Required(ErrorMessage = "Username is required")]
-        [MinLength(2, ErrorMessage = "Username should be 2-30 characters"), MaxLength(30, ErrorMessage = "Username should be 2-30 characters")]
+        [MinLength(2, ErrorMessage = "Username should contain 2-30 characters")]
+        [MaxLength(30, ErrorMessage = "Username should contain 2-30 characters")]
+        [DisplayName("Username")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
-        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password), MinLength(6), MaxLength(30)]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password should contain 6-30 characters")]
+        [MaxLength(30, ErrorMessage = "Password should contain 6-30 characters")]
+        [DisplayName("Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        public string RepeatPassword { get; set; }
+        [Compare("Password", ErrorMessage = "The password and the confirmation password do not match.")]
+        [DisplayName("Confirm password")]
+        public string ConfirmPassword { get; set; }
     }
 }
