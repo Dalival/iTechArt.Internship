2a9df93733d8d556a7bf66f53d6be9d0541edf82
Egor <egorfedorenko.w@gmail.com>
2021-10-29 14:26:48 +0300

    Add CreateSurvey page (preview version)

---
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/createSurvey.js
@@ -0,0 +1,7 @@
+﻿for (const button of $('#questions-menu').children) {
+    $(button).click(function () {
+        $('#questions-counter').html(function (i, val) {
+            return val * 1 + 1
+        });
+    })
+}
