8870bd31d6a9be3116422e58c1a336afc676af3d
Egor <egorfedorenko.w@gmail.com>
2021-10-29 15:14:38 +0300

    Fix JS code with incrementing questions counter

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/createSurvey.js
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/createSurvey.js
@@ -1,4 +1,5 @@
-﻿for (const button of $('#questions-menu').children) {
+﻿var questionCreationButtons = Array.from(document.getElementById('questions-menu').children);
+for (const button of questionCreationButtons) {
     $(button).click(function () {
         $('#questions-counter').html(function (i, val) {
             return val * 1 + 1
