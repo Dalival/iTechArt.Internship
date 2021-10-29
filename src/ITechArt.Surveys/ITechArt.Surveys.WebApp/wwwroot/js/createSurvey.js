76c4d870b78c3b7508734cfe6568374e32327b09
Egor <egorfedorenko.w@gmail.com>
2021-10-29 15:21:41 +0300

    Replace js code with jQuery code in createSurvey.js

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/createSurvey.js
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/createSurvey.js
@@ -1,8 +1,7 @@
-﻿var questionCreationButtons = Array.from(document.getElementById('questions-menu').children);
-for (const button of questionCreationButtons) {
-    $(button).click(function () {
+﻿$('#questions-menu').children().each(function () {
+    $(this).click(function () {
         $('#questions-counter').html(function (i, val) {
             return val * 1 + 1
         });
     })
-}
+})
