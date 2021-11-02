4310387876906de3669a1210172b44e10ddbf3b9
Egor <egorfedorenko.w@gmail.com>
2021-11-02 10:00:42 +0300

    View mode reflect changes from edit mode

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/createSurvey.js
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/createSurvey.js
@@ -1,4 +1,14 @@
-﻿$('#questions-menu').children().each(function () {
+﻿// Prevent submitting form on Enter
+$('#survey-form').on('keyup keypress', function(e) {
+    var keyCode = e.keyCode || e.which;
+    if (keyCode === 13 && !$(e.target).is('textarea')) {
+        e.preventDefault();
+        return false;
+    }
+});
+
+// Increment question counter on creating new question
+$('#questions-menu').children().each(function () {
     $(this).click(function () {
         $('#questions-counter').html(function (i, val) {
             return val * 1 + 1
@@ -6,6 +16,7 @@
     })
 })
 
+// Insert new question html code on click creating new question
 $('#create-text-question').click(function () {
     var i = $('.question').length;
     $.ajax({
@@ -15,7 +26,7 @@ $('#create-text-question').click(function () {
             $('#survey-content').append(data);
         },
         error: function (a, b, c) {
-            alert(a + " " + b + " " + c);
+            alert(a + ' ' + b + ' ' + c);
         }
     })
 })
---
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/textQuestion.js
@@ -0,0 +1,23 @@
+﻿// Update information in view-mode on modifying question from edit-mode
+// $('input[id^=Questions_][id$=__Title]').onchange(function () {
+//     var enteredText = $(this).val();
+//     var index = $(this).id().split('_').eq(1);
+//     $('#question-' + index + '-title').html(enteredText);
+// })
+
+// Update information in view-mode on modifying question from edit-mode
+// РАААААБОООЧИИИЙ ВАРИАААААНТ
+$('input[id^=Questions_][id$=__Title]').change(function () {
+    alert('onchange');
+    var enteredText = $(this).val();
+    var id = $(this).attr('id');
+    var collection = id.split('_');
+    var index = collection[1];
+    $('#question-' + index + '-title').html(enteredText);
+})
+
+// $('input[id^=Questions_][id$=__Title]').onkeyup(function() {
+//     var index = $(this).id().split('_').eq(1);
+//     var value = this.val();
+//     $('#question-' + index + '-title').text(value);
+// })
