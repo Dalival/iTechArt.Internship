8ae28f8d7e450a76fbb6d98090f5047661fb06c6
Egor <egorfedorenko.w@gmail.com>
2021-11-02 19:52:23 +0300

    Bug fix
    Clicks on other questions didn't switch between edit/view mode of current question. Now it's fixed.

--- src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/textQuestion.js
+++ src/ITechArt.Surveys/ITechArt.Surveys.WebApp/wwwroot/js/textQuestion.js
@@ -1,30 +1,33 @@
-﻿$(document).click(function (e) {
+﻿// On click somewhere else view view-mode
+$(document).click(function (e) {
     const target = e.target;
-    if (!$(target).is('.question-edit-mode') &&
-        !$(target).parents().is('.question-edit-mode') &&
-        !$(target).is('span[id^=edit-question-]') &&
-        !$(target).parents().is('span[id^=edit-question-]')) {
-        alert('switch to view mode');
-        $('.question-edit-mode').hide();
-        $('.question-view-mode').show();
+    const questionsCount = $('.question').length;
+    for (let i = 0; i < questionsCount; i++) {
+        if (!$(target).is(`#question-${i}`) &&
+            !$(target).parents().is(`#question-${i}`)) {
+            $(`#question-${i}-edit-mode`).hide();
+            $(`#question-${i}-view-mode`).show();
+        }
     }
 });
 
+// View edit-mode on click on edit button
 $('span[id^=edit-question-]').click(function () {
     const id = $(this).attr('id');
     const index = id.substring(id.lastIndexOf('-') + 1);
-    alert(`switch to edit mode at question ${index}`);
     $('#question-' + index + ' > .question-view-mode').hide();
     $('#question-' + index + ' > .question-edit-mode').show();
 });
 
+// Fill view-mode on changing edit-mode
 $('input[id^=Questions_][id$=__Title]').change(function () {
     let enteredText = $(this).val();
     const id = $(this).attr('id');
     const collection = id.split('_');
     const index = collection[1];
-    if(enteredText === '') {
-        enteredText = `Question ${index}`;
+    if (enteredText.trim() === '') {
+        const normalizedIndex = Number(index) + 1;
+        enteredText = 'Question ' + normalizedIndex;
     }
     $('#question-' + index + '-title').html(enteredText);
 })
